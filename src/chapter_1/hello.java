ce0d320f2955602bc69d519219029b9fc289830b
FirstName LastName <csaglik49@gmail.com>
2022-12-18 23:28:31 +0300

    added more work

--- src/chapter_1/hello.java
+++ src/chapter_1/hello.java
@@ -5,7 +5,7 @@ public class hello {
     public static void main(String[] args) {
         System.out.println("wor1 started");
         System.out.println("created Git repository");
-
+        System.out.println("made few commits");
     }
 
 }

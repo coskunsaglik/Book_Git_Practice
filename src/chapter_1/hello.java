70b76a492202f71662f77708fafba4ec5d254c2c
FirstName LastName <csaglik49@gmail.com>
2022-12-19 23:57:54 +0300

    Hello commit

--- src/chapter_1/hello.java
+++ src/chapter_1/hello.java
@@ -7,6 +7,7 @@ public class hello {
         System.out.println("Created Git repository");
         System.out.println("made few commits");
         System.out.println("made few commits");
+        System.out.println("made few commits");
     }
 
 }

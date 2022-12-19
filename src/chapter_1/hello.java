ec6f608f69a4908a4f235b598822a21dc123cdc5
FirstName LastName <csaglik49@gmail.com>
2022-12-19 22:31:10 +0300

    added more work

--- src/chapter_1/hello.java
+++ src/chapter_1/hello.java
@@ -3,8 +3,8 @@ package chapter_1;
 public class hello {
 
     public static void main(String[] args) {
-        System.out.println("wor1 started");
-        System.out.println("created Git repository");
+        System.out.println("Work1 started");
+        System.out.println("Created Git repository");
         System.out.println("made few commits");
     }
 
